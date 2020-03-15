5a54dc47e2e3e3f4717c000d8819c8fdf9673bcd
Klim Savanovich <k.sa@onerep.com>
2020-03-15 23:06:48 +0300

    deleted unnecessary files and add dynamics to the popup menu

--- shopinglist/src/layouts/MainLayout.vue
+++ shopinglist/src/layouts/MainLayout.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="app-main-layout">
 
-        <Navbar />
-        <Sidebar />
+        <Navbar @click="isOpen = !isOpen" />
+        <Sidebar v-model="isOpen" />
 
-        <main class="app-content">
+        <main class="app-content" :class="{full: !isOpen}">
             <div class="app-page">
                 <router-view/>
             </div>
@@ -24,6 +24,9 @@
 
     export default {
         name: 'main-layout',
+        data: () => ({
+            isOpen: true,
+        }),
         components: {
             Navbar, Sidebar
         }
